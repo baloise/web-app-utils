0ce19bd6c00cc1549465d37076d74ce3160f12fe
Yannick Holzenkamp <yannick@holzenkamp.me>
2021-10-08 14:20:29 +0200

    fix(): footer links

--- src/footer.ts
+++ src/footer.ts
@@ -1,11 +1,11 @@
-import { ENGLISH, FRENCH, GERMAN, ITALIAN, Language } from ".";
+import { ENGLISH, Language } from ".";
 
-const URLS = new Map([
-  [GERMAN.key, 'https://www.baloise.ch/de/privatkunden.json'],
-  [ENGLISH.key, 'https://www.baloise.ch/en/private-customers.json'],
-  [FRENCH.key, 'https://www.baloise.ch/fr/clients-prives.json'],
-  [ITALIAN.key, 'https://www.baloise.ch/it/clienti-privati.json']
-])
+const MAGNOLIA_API_URL = new Map([
+  ['de', 'https://www.baloise.ch/de/privatkunden.json'],
+  ['fr', 'https://www.baloise.ch/fr/clients-prives.json'],
+  ['it', 'https://www.baloise.ch/it/clienti-privati.json'],
+  ['en', 'https://www.baloise.ch/en/private-customers.json']
+]);
 
 export interface FooterLink {
   label: string;
@@ -13,12 +13,9 @@ export interface FooterLink {
 };
 
 export const loadFooterLinks = (lang: Language): Promise<FooterLink[]> => {
-  if (URLS.has(lang.key)) {
-    return fetch(URLS.get(lang.key))
-    .then(res => res.json())
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    .then(res => Object.values(res[0]['footer']['footerMetaList']));
-  } else {
-    throw new Error('Footer links can not be fetched for language: ' + JSON.stringify(lang));
-  }
+  const url = MAGNOLIA_API_URL.has(lang.key) ? MAGNOLIA_API_URL.get(lang.key) : MAGNOLIA_API_URL.get(ENGLISH.key);
+  return fetch(url)
+  .then(res => res.json())
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  .then(res => Object.values(res[0]['footer']['footerMetaList']));
 };
