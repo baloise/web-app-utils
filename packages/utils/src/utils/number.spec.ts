084941dcb2c1d10c2da2f590d90453a4d6fc9c33
hirsch <gerhard.hirschfeld@baloise.ch>
2022-02-18 13:32:35 +0100

    feat(utils): add local number functions

--- packages/utils/src/utils/number.spec.ts
+++ packages/utils/src/utils/number.spec.ts
@@ -1,10 +1,52 @@
-import { isValidMonetaryNumber } from './number'
+import {
+  isValidMonetaryNumber,
+  getDecimalSeparator,
+  getThousandSeparator,
+  formatLocaleNumber,
+  parseLocaleNumber,
+} from './number'
 
-describe('isValidMonetaryNumber', () => {
-  test('should return true if it is a valid monetary number', () => {
-    expect(isValidMonetaryNumber('')).toBe(false)
-    expect(isValidMonetaryNumber('a')).toBe(false)
-    expect(isValidMonetaryNumber('-85')).toBe(true)
-    expect(isValidMonetaryNumber("1'000.98")).toBe(true)
+describe('number', () => {
+  describe('isValidMonetaryNumber', () => {
+    test('should return true if it is a valid monetary number', () => {
+      expect(isValidMonetaryNumber('')).toBe(false)
+      expect(isValidMonetaryNumber('a')).toBe(false)
+      expect(isValidMonetaryNumber('-85')).toBe(true)
+      expect(isValidMonetaryNumber("1'000.98")).toBe(true)
+    })
+  })
+
+  describe('getDecimalSeparator', () => {
+    test('should return the correct decimal separator in the given locale', () => {
+      expect(getDecimalSeparator('de-CH')).toBe('.')
+      expect(getDecimalSeparator('de-DE')).toBe(',')
+    })
+  })
+
+  describe('getThousandSeparator', () => {
+    test('should return the correct thousand separator in the given locale', () => {
+      expect(getThousandSeparator('de-CH')).toBe('’')
+      expect(getThousandSeparator('de-DE')).toBe('.')
+    })
+  })
+
+  describe('formatLocaleNumber', () => {
+    test('should format the number into the given locale', () => {
+      expect(formatLocaleNumber('de-CH', 1000.42)).toBe('1’000.42')
+      expect(formatLocaleNumber('de-DE', 1000.42)).toBe('1.000,42')
+      expect(formatLocaleNumber('de-DE', 0)).toBe('0')
+      expect(formatLocaleNumber('de-DE', -1)).toBe('-1')
+      expect(formatLocaleNumber('de-DE', NaN)).toBe('')
+      expect(formatLocaleNumber('de-DE', undefined)).toBe('')
+    })
+  })
+
+  describe('parseLocaleNumber', () => {
+    test('should parse the locale number into the native number', () => {
+      expect(parseLocaleNumber('de-CH', '1’000.42')).toBe(1000.42)
+      expect(parseLocaleNumber('de-DE', '1.000,42')).toBe(1000.42)
+      expect(parseLocaleNumber('de-CH', '')).toBe(NaN)
+      expect(parseLocaleNumber('de-CH', '-1')).toBe(-1)
+    })
   })
 })
--- packages/utils/src/utils/number.ts
+++ packages/utils/src/utils/number.ts
@@ -21,3 +21,67 @@ export function isValidMonetaryNumber(stringValue: string): boolean {
 
   return !isNaN(parseFloat(stringValue))
 }
+
+/**
+ * Returns the decimal separator of the given locale
+ *
+ * ```typescript
+ * getDecimalSeparator('de-ch') // .
+ * ```
+ */
+export const getDecimalSeparator = (locale = 'de-CH'): string => {
+  return Intl.NumberFormat(locale)
+    .format(1.1)
+    .replace(/\p{Number}/gu, '')
+}
+
+/**
+ * Returns the thousand separator of the given locale
+ *
+ * ```typescript
+ * getThousandSeparator('de-ch') // '
+ * ```
+ */
+export const getThousandSeparator = (locale = 'de-CH'): string => {
+  return Intl.NumberFormat(locale)
+    .format(11111)
+    .replace(/\p{Number}/gu, '')
+}
+
+/**
+ * Formats the number into the given locale
+ *
+ * ```typescript
+ * formatLocaleNumber('de-ch', 1000.42) // 1'000.42
+ * ```
+ */
+export const formatLocaleNumber = (locale = 'de-CH', number: number, minimumFractionDigits?: number): string => {
+  const options = minimumFractionDigits !== undefined ? { minimumFractionDigits } : {}
+  const formattedNumber = Intl.NumberFormat(locale, {
+    ...options,
+  }).format(number)
+
+  if (formattedNumber === 'NaN') {
+    return ''
+  }
+
+  return formattedNumber
+}
+
+/**
+ * Parses the locale formatted number into a native number
+ *
+ * ```typescript
+ * parseLocaleNumber('de-ch', '1'000.42') // 1000.42
+ * ```
+ */
+export const parseLocaleNumber = (locale = 'de-CH', stringNumber: string): number => {
+  const thousandSeparator = getThousandSeparator(locale)
+  const decimalSeparator = getDecimalSeparator(locale)
+
+  return parseFloat(
+    stringNumber
+      .replace(new RegExp('\\' + thousandSeparator, 'g'), '')
+      .replace(new RegExp('\\' + decimalSeparator), '.'),
+  )
+}
