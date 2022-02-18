9a92609cdb64b8d077ae6c90448230bf40658b42
hirsch <gerhard.hirschfeld@baloise.ch>
2022-02-18 14:07:48 +0100

    chore: rename date string functions

--- packages/pipes/src/balDateString.spec.ts
+++ packages/pipes/src/balDateString.spec.ts
@@ -1,6 +1,6 @@
 import { balDateString } from './balDateString'
 
-describe('balDataString', () => {
+describe('balDateString', () => {
   it('should be able to handle null and undefined', () => {
     expect(balDateString(new Date(2022, 11, 31))).toBe('2022-12-31')
   })
