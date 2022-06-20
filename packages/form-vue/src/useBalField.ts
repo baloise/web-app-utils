c543aa1f9d0fc71dbd8b386c11cc73cde6048c8a
hirsch <gerhard.hirschfeld@baloise.ch>
2022-06-20 13:30:52 +0200

    fix(vue-form): refactor useBalField

--- packages/form-vue/src/useBalField.ts
+++ packages/form-vue/src/useBalField.ts
@@ -1,57 +1,29 @@
-import { FieldContext, useField } from 'vee-validate'
-import { computed, ComputedRef, ref, Ref, watch } from 'vue'
-import { ValidationMessage } from './yup/types.yup'
+import { useFieldError } from 'vee-validate'
+import { computed, Ref } from 'vue'
 
 export type MaybeRef<T> = Ref<T> | T
 
-export interface BalFieldProps {
-  value?: string
-  label?: string
-  disabled?: boolean
-  message?: string
-}
-
-export interface BalFieldContext<TValue> extends FieldContext<TValue> {
-  invalid: Ref<boolean>
-  message: ComputedRef<ValidationMessage>
-}
-
-export interface BalFieldOptions {
-  label?: MaybeRef<string>
-  props?: Readonly<unknown>
-  propKey?: string
-}
-
-export function useBalField<TValue>(name: MaybeRef<string>, options: BalFieldOptions = {}): BalFieldContext<TValue> {
-  const field = useField<TValue>(name, undefined, { label: options.label })
-
-  const invalid = computed(() => !!field.errorMessage.value)
-  const validMessage = ref<string>('')
-
-  if (options && options.props) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const p = options.props as any
-    const k = options.propKey || 'value'
-    validMessage.value = p['message']
-    watch(
-      () => p[k],
-      () => field.setValue(p[k]),
-    )
-    watch(
-      () => p['message'],
-      () => (validMessage.value = p['message']),
-    )
+const isYupError = (a: any) => {
+  if ((typeof a === 'object' || typeof a === 'function') && a !== null) {
+    if ('key' in a) {
+      return true
+    }
   }
+  return false
+}
+
+export function useBalField<TValues = unknown>(path: MaybeRef<string>, message?: MaybeRef<string>) {
+  const error = useFieldError(path)
+
+  const validationMessageKey = computed(() => (isYupError(error.value) ? (error as any).value.key : error.value || ''))
+
+  const validationValues = computed<TValues>(() => (isYupError(error.value) ? (error as any).value.values : {}))
+
+  const invalid = computed(() => !!error.value)
 
   return {
-    ...field,
     invalid,
-    message: computed<ValidationMessage>(() =>
-      invalid.value
-        ? (field.errorMessage.value as any) || ''
-        : validMessage.value
-        ? { key: validMessage.value, values: {} }
-        : undefined,
-    ),
+    message: computed<string>(() => (invalid.value ? validationMessageKey.value : message || '')),
+    validationValues,
   }
 }
