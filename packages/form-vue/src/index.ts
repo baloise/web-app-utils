43afbabc66f9a103626944192c8fe61927e45be6
hirsch <gerhard.hirschfeld@baloise.ch>
2022-06-20 13:33:13 +0200

    fix(vue-form): refactor useBalField

--- packages/form-vue/src/index.ts
+++ packages/form-vue/src/index.ts
@@ -1,9 +1,8 @@
 import type { ValidationMessage } from './yup/types.yup'
-import type { BalFieldProps } from './useBalField'
 
 import { baloiseForm } from './plugin'
 import { useBalField } from './useBalField'
 
-export type { ValidationMessage, BalFieldProps }
+export type { ValidationMessage }
 
 export { baloiseForm, useBalField }
