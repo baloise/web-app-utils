64d72c4506a9d40727e51af23b2ae794bd5532d3
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2023-01-24 08:03:02 +0100

    feat(utils): add germany footer links

--- packages/utils/src/integration/footer.ts
+++ packages/utils/src/integration/footer.ts
@@ -1,10 +1,16 @@
-import { Language, LanguagesOfSwitzerland } from '../language/index'
+import { Language, LanguagesOfGermany, LanguagesOfSwitzerland } from '../language/index'
 
-const MAGNOLIA_API_URL = new Map([
+type Region = 'CH' | 'DE'
+
+const MAGNOLIA_API_URL_CH = new Map<string, string>([
   ['de', 'https://www.baloise.ch/app-integration/v2/footer/de.json'],
   ['fr', 'https://www.baloise.ch/app-integration/v2/footer/fr.json'],
   ['it', 'https://www.baloise.ch/app-integration/v2/footer/it.json'],
   ['en', 'https://www.baloise.ch/app-integration/v2/footer/en.json'],
+])
+
+const MAGNOLIA_API_URL_DE = new Map<string, string>([
+  ['de', 'https://www.baloise.ch/app-integration/v2/de/footer/de.json'],
 ])
 
 export interface FooterLink {
@@ -12,9 +18,17 @@ export interface FooterLink {
   link: string
 }
 
-export const loadFooterLinks = (lang: Language): Promise<FooterLink[]> => {
-  const effectiveLang = LanguagesOfSwitzerland.valueOfOrDefault(lang ? lang.key : undefined)
-  const url = MAGNOLIA_API_URL.get(effectiveLang.key)
+export const loadFooterLinks = (lang: Language, region: Region = 'CH'): Promise<FooterLink[]> => {
+  let url = MAGNOLIA_API_URL_CH.get('de')
+
+  if (region === 'CH') {
+    const effectiveLang = LanguagesOfSwitzerland.valueOfOrDefault(lang ? lang.key : undefined)
+    url = MAGNOLIA_API_URL_CH.get(effectiveLang.key)
+  } else if (region === 'DE') {
+    const effectiveLang = LanguagesOfGermany.valueOfOrDefault(lang ? lang.key : undefined)
+    url = MAGNOLIA_API_URL_DE.get(effectiveLang.key)
+  }
+
   return fetch(url)
     .then(res => res.json())
     .then(res => res as FooterLink[])
