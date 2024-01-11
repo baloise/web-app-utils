a78c7cf33deaa0819c74cdaf07658c86ee7bf671
Predrag <>
2024-01-11 14:34:56 +0100

    fix(NLP-12627): prettier and not used fix

--- packages/pipes/src/balHighlight.ts
+++ packages/pipes/src/balHighlight.ts
@@ -7,10 +7,8 @@
  */
 export function balHighlight(value: string, search: string, cssClass = 'bal-highlight'): string {
   if (search && value) {
-    let hrefTag = '(href=\"[^>]+\")|('
-    let pattern = hrefTag
-      .concat(search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&'))
-      .concat(')')
+    const hrefTag = '(href="[^>]+")|('
+    let pattern = hrefTag.concat(search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')).concat(')')
     pattern = pattern
       .split(' ')
       .filter(t => {
@@ -19,7 +17,7 @@ export function balHighlight(value: string, search: string, cssClass = 'bal-high
       .join('|')
     const regex = new RegExp(pattern, 'gi')
     return value.replace(regex, match => {
-      return match.includes('href') ? match : `<span class="bal-highlight">${match}</span>`
+      return match.includes('href') ? match : `<span class="${cssClass}">${match}</span>`
     })
   } else {
     return value
