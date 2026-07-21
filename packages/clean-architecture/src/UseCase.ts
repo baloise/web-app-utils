3543f592ae81cad54fc62eff4bd430bd64cda74d
Christian Mitev <christianmitev1@gmail.com>
2026-07-21 07:35:22 +0200

    deleted unused packages

--- packages/clean-architecture/src/UseCase.ts
+++
@@ -1,5 +0,0 @@
-import { Result } from './Result'
-
-export interface UseCase<Context, TValue, TError = string> {
-  execute(context: Context): Promise<Result<TValue, TError>>
-}
--- packages/form-vue/src/yup/types.yup.ts
+++
@@ -1,9 +0,0 @@
-export type ValidationMessageValues<TValue = unknown, TArg = unknown> = {
-  value: TValue
-  arg: TArg
-}
-
-export type ValidationMessage<TValue = unknown, TArg = unknown> = {
-  key: string
-  values: ValidationMessageValues<TValue, TArg>
-}
